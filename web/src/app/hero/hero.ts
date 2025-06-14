0beaa698374898ec1ef1bbd3d14cddda7a5147a9
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-06-14 19:15:52 +0200

    video hero

--- web/src/app/hero/hero.ts
+++ web/src/app/hero/hero.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
   templateUrl: './hero.html',
-  styleUrl: './hero.scss'
+  styleUrl: './hero.scss',
+  standalone: true
 })
-export class Hero {
+export class Hero implements AfterViewInit {
+  @ViewChild('heroVideo') heroVideo!: ElementRef<HTMLVideoElement>;
 
+  ngAfterViewInit() {
+    if (this.heroVideo && this.heroVideo.nativeElement) {
+      this.heroVideo.nativeElement.muted = true;
+      this.heroVideo.nativeElement.play().catch(() => {});
+    }
+  }
 }
