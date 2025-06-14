f9af7531323e9830d73dbcebe0cf7ea90aba5bb3
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-06-14 20:14:37 +0200

    footer

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Hero } from './hero/hero';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
