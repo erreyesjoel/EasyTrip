d2d7f81744edff9e702639f08034d47858aa8cd4
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-06-17 19:56:23 +0200

    añadir un modo recuperacion y poner comentarios explicativos

--- web/src/app/iniciar-sesion/iniciar-sesion.ts
+++ web/src/app/iniciar-sesion/iniciar-sesion.ts
@@ -8,13 +8,21 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule]
 })
 export class IniciarSesion {
-  modoRegistro = false;
+  modoRegistro = false; // modo registro false, porque por defecto se muestra el login
+  modoRecuperacion = false; // modo recuperacion false, porque por defecto se muestra el login
 
   mostrarRegistro() {
-    this.modoRegistro = true;
+    this.modoRegistro = true; // modo registro true, porque se ha pulsado el boton de registro
+    this.modoRecuperacion = false; // modo recuperacion false, porque se ha pulsado el boton de registro, y NO el de recuperacion
   }
 
   mostrarLogin() {
-    this.modoRegistro = false;
+    this.modoRegistro = false; // modo registro false, porque se ha pulsado el boton de login
+    this.modoRecuperacion = false; // modo recuperacion false, porque se ha pulsado el boton de login, y NO el de recuperacion
+  }
+
+  mostrarRecuperacion() {
+    this.modoRegistro = false; // modo registro false, porque se ha pulsado el boton de recuperacion, y NO el de registro
+    this.modoRecuperacion = true; // modo recuperacion true, porque se ha pulsado el boton de recuperacion
   }
 }
