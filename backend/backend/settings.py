a0880ae9ed37a27c7ce7a009e51932e86ec75951
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-06-19 15:01:36 +0200

    envio de correos reales gmail

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,3 +142,12 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+APP_NAME = env('APP_NAME')
