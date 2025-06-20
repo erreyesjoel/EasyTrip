2da8043224b92f9766315509a6ce61777cac23ee
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-06-20 23:14:05 +0200

    roles y nombre de la tabla cambiado

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,3 +3,14 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+
+    ROL_CHOICES = [
+        ('cliente','Cliente'),
+        ('agente', 'Agente'),
+        ('recepcionista','Recepcionista'),
+        ('administrador','Administrador'),
+    ]
+    rol = models.CharField(max_length=20, choices=ROL_CHOICES, default='cliente')
+
+    class Meta:
+        db_table = 'usuarios'
