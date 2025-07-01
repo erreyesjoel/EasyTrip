36b8072c80d662d214e881055f7c7e77fdae3de7
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-07-01 18:08:52 +0200

    cuando haya un usuario nuevo en la base de datos, se le rellenara automaticamente el campo last login al logearse

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,7 @@
+from datetime import timezone
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
@@ -12,5 +14,13 @@ class CustomUser(AbstractUser):
     ]
     rol = models.CharField(max_length=20, choices=ROL_CHOICES, default='cliente')
 
+    # actualizar el campo last_login al guardar el usuario, SI last_login esta vacio
+    # se establece la fecha y hora actual
+    # self es la instancia del usuario que se esta guardando
+    # args y kwargs permiten que el metodo acepte cualquier argumento adicional que Django le pase internamente al guardar el modelo
+    def save(self, *args, **kwargs):
+        if not self.last_login:
+            self.last_login = timezone.now()
+            super().save(*args, **kwargs)
     class Meta:
         db_table = 'usuarios'
