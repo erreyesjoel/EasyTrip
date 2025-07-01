2a2403a04c2f6b2944d5dd0421e2d49aa1e3751d
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-07-01 19:28:23 +0200

    el ts ahora si muestra los usuarios de la base de datos mediante la api

--- web/src/app/gestion-usuarios/gestion-usuarios.ts
+++ web/src/app/gestion-usuarios/gestion-usuarios.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SidebarComponent } from '../sidebar/sidebar';
 import { environment } from '../../environments/environment';
+
 
 // Interfaz para el usuario
 // Representa la estructura de un usuario en el sistema
@@ -9,17 +11,17 @@ interface Usuario {
   username: string;
   email: string;
   rol: string;
-  estado: string;
+  is_active: boolean;
   first_name: string;
   last_name: string;
   last_login: Date;
-  data_joined: Date;
+  date_joined: Date;
 
 }
 
 @Component({
   selector: 'app-gestion-usuarios',
-  imports: [SidebarComponent],
+  imports: [CommonModule, SidebarComponent],
   templateUrl: './gestion-usuarios.html',
   styleUrls: ['./gestion-usuarios.scss']
 })
@@ -28,12 +30,21 @@ export class GestionUsuarios {
 
   // cuando se "renderiza el componente", se hara peticion a la api gestion-usuarios
   // por eso ngOnInit porque es el ciclo de vida del componente que se ejecuta al inicializar
-  async ngOnInit() {
+
+  usuarios: Usuario[] = [];
+
+  ngOnInit() {
+      this.cargarUsuarios();
+  }
+
+  /* Carga los usuarios desde la API 
+  funcion independiente, que llamaremos desde ngOnInit */
+  async cargarUsuarios(): Promise<void> {
     const res = await fetch(environment.apiBaseUrl + 'gestion-usuarios');
-    // si la respuesta de la api gestion-usuarios es exitosa (200)
-    // consultaremos los usuarios con depuracion en console.log
     if (res.status === 200) {
       const usuarios: Usuario[] = await res.json();
+      this.usuarios = usuarios; // 
+      // Ahora this.usuarios contiene los usuarios obtenidos de la API
       console.log("Usuarios de la bbdd:", usuarios);
     } else {
       console.log("Error en la peticion");
