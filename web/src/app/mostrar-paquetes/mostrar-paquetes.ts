4e8e15dbba4c346632e1971bd19bb0c2b6c0c7b6
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-07-03 19:21:18 +0200

    peticion a la api para obtener los paquetes en la landing page, pero solo por console log para testear

--- web/src/app/mostrar-paquetes/mostrar-paquetes.ts
+++ web/src/app/mostrar-paquetes/mostrar-paquetes.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-mostrar-paquetes',
@@ -9,5 +10,22 @@ import { Component } from '@angular/core';
 export class MostrarPaquetes {
   constructor() {
     console.log('MostrarPaquetes component se esta ejecutando');
+  }
+
+  async ngOnInit() {
+    console.log("ngOnInit, prueba");
+    await this.mostrarPaquetesLanding(); // llamamos a la función para mostrar paquetes, nada mas inicializar el componente
+  }
+
+  async mostrarPaquetesLanding(): Promise<void> {
+    try {
+      const res = await fetch(environment.apiBaseUrl + 'paquetes?estado=activo');
+      if (res.status === 200) {
+        const paquetes = await res.json(); // convertimos la respuesta a JSON
+        console.log("Paquetes activos:", paquetes); // depuracion para probar, que funcione la peticion get a la api
+      }
+    } catch (error: any) {
+      console.error('Error al cargar los paquetes:', error);
+    }
   }
 }
