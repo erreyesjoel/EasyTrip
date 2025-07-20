872293e52e0724b7a504297e348ab97a28e96715
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-07-20 22:27:45 +0200

    Integrar el componente Sidebar en la gestión de reservas y actualizar la ruta en el menú de navegación

--- web/src/app/gestion-reservas/gestion-reservas.ts
+++ web/src/app/gestion-reservas/gestion-reservas.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SidebarComponent} from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-gestion-reservas',
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './gestion-reservas.html',
   styleUrl: './gestion-reservas.scss'
 })
