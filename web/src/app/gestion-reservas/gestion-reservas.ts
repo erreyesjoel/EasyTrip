763d2261af19e28170230755723faae1ce6230ff
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-07-29 21:44:38 +0200

    mostrar reservas en la tabla, he codigo el scss tal tual de gestion usuarios, por eso hay algunas clases con el mismo nombre, los cambiare

--- web/src/app/gestion-reservas/gestion-reservas.ts
+++ web/src/app/gestion-reservas/gestion-reservas.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SidebarComponent} from '../sidebar/sidebar';
 import { environment } from '../../environments/environment';
+import { CommonModule } from '@angular/common';
 
 interface Reserva {
   id: number;
@@ -13,12 +14,18 @@ interface Reserva {
 
 @Component({
   selector: 'app-gestion-reservas',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, CommonModule],
   templateUrl: './gestion-reservas.html',
   styleUrl: './gestion-reservas.scss'
 })
 export class GestionReservas {
 
+  // array en el que se almacenan las reservas
+  // declarado en el for each, ngFor en el html
+  reservas: Reserva[] = [];
+
+  // ngOnInit porque se utiliza para inicializar la carga de datos al inicio del componente
+  // nada mas renderizar el componente, llamamos de forma asincrona a la funcion obtenerReservas
   async ngOnInit() {
     await this.obtenerReservas();
   }
@@ -31,6 +38,7 @@ export class GestionReservas {
       // si la respuesta es exitosa, 200, como es get
       if (res.status === 200) {
         const data = await res.json();
+        this.reservas = data; // Asignar los datos a la propiedad reservas
         // console log para depuracion
         console.log('Reservas obtenidas:', data);
       }
