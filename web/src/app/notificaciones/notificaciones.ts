f9b88413c9b27abb315d1e4a2a74bbf5c186921f
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-08-02 23:19:44 +0200

    Add Notificaciones component with template and styles

---
+++ web/src/app/notificaciones/notificaciones.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-notificaciones',
+  imports: [],
+  templateUrl: './notificaciones.html',
+  styleUrl: './notificaciones.scss'
+})
+export class Notificaciones {
+
+}
