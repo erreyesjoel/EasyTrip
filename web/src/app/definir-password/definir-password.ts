72cb33b777f239a140627e1f7a2acd6466257cae
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-08-14 02:39:03 +0200

    definicion de password, desde front ya funciona, faltan validaciones y tal, pero de funcionar ya funciona

--- web/src/app/definir-password/definir-password.ts
+++ web/src/app/definir-password/definir-password.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-definir-password',
-  imports: [],
   templateUrl: './definir-password.html',
-  styleUrl: './definir-password.scss'
+  styleUrl: './definir-password.scss',
+  standalone: true,
+  imports: [FormsModule] 
 })
 export class DefinirPassword {
+  password = '';
+  confirmPassword = '';
 
+  getParamsFromUrl(): { user_id: string, token: string } {
+    const params = new URLSearchParams(window.location.search);
+    const user_id = params.get('user_id') || '';
+    const token = params.get('token') || '';
+    console.log('URL params:', { user_id, token });
+    return { user_id, token };
+  }
+
+  async guardarPassword() {
+    const { user_id, token } = this.getParamsFromUrl();
+    console.log('Intentando enviar:', { user_id, token, password: this.password, confirmPassword: this.confirmPassword });
+
+    if (!user_id || !token || !this.password || !this.confirmPassword) {
+      console.log('Faltan datos');
+      return;
+    }
+    if (this.password !== this.confirmPassword) {
+      console.log('Las contraseñas no coinciden');
+      return;
+    }
+
+    try {
+      const res = await fetch(environment.apiBaseUrl + `definicion-password/${user_id}/`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          token,
+          password: this.password
+        })
+      });
+      console.log('Respuesta HTTP:', res.status);
+      const data = await res.json().catch(() => ({}));
+      console.log('Respuesta JSON:', data);
+    } catch (err) {
+      console.error('Error en fetch:', err);
+    }
+  }
 }
