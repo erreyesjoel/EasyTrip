2b830403c995273d7a64e69a3ed3dc68bea83c08
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-08-20 04:53:39 +0200

    por ahora solo mostrar el total de usuarios en le dashboard

--- web/src/app/dashboard/dashboard.ts
+++ web/src/app/dashboard/dashboard.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from '../sidebar/sidebar'; // Usa la ruta correcta según tu estructura
+import { environment } from '../../environments/environment';
+
+interface UsuariosCountResponse {
+  total: number;
+  activos: number;
+  inactivos: number;
+  agentes: number;
+  clientes: number;
+  administradores: number;
+}
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -8,4 +19,24 @@ import { SidebarComponent } from '../sidebar/sidebar'; // Usa la ruta correcta s
   templateUrl: './dashboard.html',   // <--- debe existir este archivo
   styleUrl: './dashboard.scss'       // <--- debe existir este archivo
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+
+  totalUsuarios: number | null = null;
+
+  // ngOnInit, cada vez que se renderia componente, llama a mostrarTotalUsuarios
+  ngOnInit():void {
+    this.mostrarTotalUsuarios();
+  }
+
+  async mostrarTotalUsuarios(): Promise<void> {
+    try {
+      const res = await fetch(environment.apiBaseUrl + 'count-usuarios');
+      if (!res.ok) throw new Error('Error al obtener el total de usuarios');
+      const data: UsuariosCountResponse = await res.json();
+      this.totalUsuarios = data.total;
+    } catch (error) {
+      this.totalUsuarios = null;
+      console.error('Error al mostrar total de usuarios:', error);
+    }
+  }
+}
