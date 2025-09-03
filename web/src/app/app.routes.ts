b7642e87f63786a5c22884755abc111f4d9ad91d
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-09-03 14:51:31 +0200

    si no estamos logeados no podemos ver la pagina de reservas del usuario, si estamos logeados si, y si somos clientes

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -12,6 +12,8 @@ import { Reserva } from './reserva/reserva';
 import { GestionReservas } from './gestion-reservas/gestion-reservas';
 import { Paquetes } from './paquetes/paquetes';
 import { DefinirPassword } from './definir-password/definir-password';
+import { ReservasUsuario } from './reservas-usuario/reservas-usuario';
+import { ClienteGuard } from './guards/cliente.guard';
 
 export const routes: Routes = [
   {
@@ -36,4 +38,7 @@ export const routes: Routes = [
   { path: 'reserva/:id/:nombre', component: Reserva },
   { path: 'paquetes', component: Paquetes }, // ruta para ver todos los paquetes
   { path: 'definir-password', component: DefinirPassword },
+  // Usar cliente guard
+  // Para que, si no estoy logeado, y no soy cliente, no deje acceder a reservas del usuario
+  { path: 'reservas', component: ReservasUsuario, canActivate: [ClienteGuard] },
 ];
---
+++ web/src/app/reservas-usuario/reservas-usuario.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservas-usuario',
+  imports: [],
+  templateUrl: './reservas-usuario.html',
+  styleUrl: './reservas-usuario.scss'
+})
+export class ReservasUsuario {
+
+}
